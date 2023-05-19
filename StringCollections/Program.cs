398b8d5885a709cf5c7b7a9f0bf358001001c8fd
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-05-19 11:59:58 +0100

    Stringcollections add list

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace StringCollections
 {
@@ -6,7 +8,16 @@ namespace StringCollections
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
+            List<string> cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Mumbai");
+            cities.Add("Chicago");
+            cities.Add("London");
+
+            Console.WriteLine();
         }
     }
 }
